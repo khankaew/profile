df1c5f408c62a360d310eae7a9cb46568fd5aff9
Thiw <pomoeng@gmail.com>
2022-06-12 17:04:44 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Page1 from './pages/page1'
+import Page2 from './pages/page2'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="page1" element={<Page1 />} />
+      <Route path="page2" element={<Page2 />} />
+    </Routes>
+  </BrowserRouter>
+);
+// root.render(
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<App />}>
+//         <Route path="/page1" element={<Page1 />} />
+//       </Route>
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ src/pages/page2.js
@@ -0,0 +1,10 @@
+
+function Page2() {
+  return (
+    <div>
+      <h1>Page2</h1>
+    </div>
+  );
+}
+
+export default Page2;
